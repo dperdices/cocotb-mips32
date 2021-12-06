45e59d12d6c13cc7807a5a5b0cc6aa143bd7ff7f
Daniel Perdices <daniel.perdices@uam.es>
2021-12-06 01:13:26 +0100

    Added docker support in the script

--- cocotb_mips32/cli.py
+++ cocotb_mips32/cli.py
@@ -5,7 +5,7 @@ import argparse
 import docker
 
 def main():
-    with tempfile.TemporaryDirectory() as dir:
+    with tempfile.TemporaryDirectory(prefix="cocotb-mips32") as tmp_dir:
         parser = argparse.ArgumentParser(description="MIPS32 test suite")
         parser.add_argument("-m","--module", help="Python module", required=True)
         parser.add_argument("-t","--toplevel", help="Top Level entity name", default="processor")
@@ -19,12 +19,12 @@ def main():
         file_stdout, docker_image = args["stdout"], args["docker_image"]
 
         if simulator == "docker-questa":
+            print("Running in docker")
             real_rtl_dir = os.path.abspath(rtl_dir)
-            rtl_dir = "/cwd/sim_build"
-            real_dir = os.path.abspath(dir)
-            dir = "/cwd/dir"
+            rtl_dir = "/rtl_dir"
+            real_tmp_dir = os.path.abspath(tmp_dir)
+            tmp_dir = "/tmp_dir"
             client = docker.from_env()
-        if simulator == "docker-questa":
             fli_lib = client.containers.run(docker_image, command="cocotb-config --lib-name-path fli questa", auto_remove=True).decode("utf8").replace("\n", "")
         else:
             fli_lib = subprocess.run(["cocotb-config", "--lib-name-path", "fli", "questa"], stdout=subprocess.PIPE).stdout.decode("utf8").replace("\n", "")
@@ -32,10 +32,10 @@ def main():
         onerror {{
         quit -f -code 1
         }}
-        if [file exists {dir}/work] {{vdel -lib {dir}/work -all}}
-        vlib {dir}/work
+        if [file exists {tmp_dir}/work] {{vdel -lib {tmp_dir}/work -all}}
+        vlib {tmp_dir}/work
         vmap -c
-        vmap work {dir}/work
+        vmap work {tmp_dir}/work
         vcom -work work +acc {rtl_dir}/*.vhd
         vsim  -onfinish exit -foreign "cocotb_init {fli_lib}" work.{toplevel}
         run -all
@@ -57,8 +57,8 @@ def main():
         
         if simulator == "docker-questa":
             vols = {
-                real_dir: {
-                    "bind": dir, "mode": "rw"
+                real_tmp_dir: {
+                    "bind": tmp_dir, "mode": "rw"
                 },
                 real_rtl_dir: {
                     "bind": rtl_dir, "mode": "ro"
@@ -73,12 +73,20 @@ def main():
             input_socket = container.attach_socket(
                 params={
                     "stdin":True,
+                    "stdout":True,
+                    "stderr":True,
                     "stream":True,
                 },
             )
-            input_socket._sock.send(do_script.encode("utf8"))
-            print(container.logs())
+            input_socket._sock.sendall(do_script.encode("utf8"))
+            input_socket._sock.close()
+            container.stop()
             container.wait()
+
+            print(input_socket, dir(input_socket))
+            print(container.logs().decode("utf8"))
+            
+            container.remove()
             
         else:
             proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, env=env, stderr=stderr, stdout=stdout)
